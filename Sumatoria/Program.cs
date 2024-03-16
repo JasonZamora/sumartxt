7709d04665e4aba7253e96b757befcfc39b63297
Jason <janci5sep@gmail.com>
2024-03-16 14:16:07 -0600

    Agregar archivos de proyecto.

---
+++ Sumatoria/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sumatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola este es el código de Jason");
+            
+        }
+    }
+}
